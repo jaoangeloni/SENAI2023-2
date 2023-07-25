d17eed81d12121a99b9bcea0fd9c8fbf66c1fd14
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-07-25 11:30:09 -0300

    PPDMO

--- PPDMO/aula01/docs/script.sql
+++ PPDMO/aula01/docs/script.sql
@@ -1,49 +1,76 @@
-DROP DATABASE IF EXISTS restaurante;
-CREATE DATABASE restaurante;
-USE restaurante;
+    DROP DATABASE IF EXISTS restaurante;
+    CREATE DATABASE restaurante;
+    USE restaurante;
 
-CREATE TABLE cardapio(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    item VARCHAR
-(20) NOT NULL,
-    preco DECIMAL
-(5,2) NOT NULL,
-    descricao VARCHAR
-(255) NOT NULL
-);
+    CREATE TABLE cardapio(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        item VARCHAR(50) NOT NULL,
+        preco DECIMAL(5,2) NOT NULL,
+        descricao VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE motoboy(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        nome varchar(50) NOT NULL,
+        moto varchar(50) NOT NULL
+    );
 
-CREATE TABLE cliente(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR
-(60) NOT NULL,
-    telefone VARCHAR
-(15) NOT NULL,
-    endereco VARCHAR
-(100) NOT NULL
-);
+    CREATE TABLE cliente(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(60) NOT NULL,
+        telefone VARCHAR(15) ,
+        endereco VARCHAR(100) 
+    );
 
-CREATE TABLE pedido(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_cliente INT NOT NULL,
-    hora TIME NOT NULL,
-    data DATE not null,
-    status VARCHAR
-(10) NOT NULL,
-    obs VARCHAR
-(255) NOT NULL,
-    FOREIGN KEY
-(id_cliente) REFERENCES cliente
-(id) 
-);
+    CREATE TABLE pedido(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        id_cliente INT NOT NULL,
+        id_motoboy INT,
+        hora_pedido TIME NOT NULL,
+        hora_fim TIME,
+        data DATE not null,
+        status VARCHAR(10) NOT NULL,
+        obs VARCHAR(255),
+        FOREIGN KEY(id_cliente) REFERENCES cliente(id),
+        FOREIGN KEY(id_motoboy) REFERENCES motoboy(id),
+        entrega BOOLEAN
+    );
 
-CREATE TABLE itens
-(
-    id_pedido INT NOT NULL,
-    id_cardapio INT NOT NULL,
-    total DECIMAL(6,2) NOT NULL,
-    descricao TEXT NOT NULL,
-    FOREIGN KEY (id_pedido) REFERENCES pedido(id)
-    FOREIGN KEY
-    (id_cardapio) REFERENCES cardapio
-    (id)
-);
+    CREATE TABLE itens
+    (
+        id_pedido INT NOT NULL,
+        id_cardapio INT NOT NULL,
+        total DECIMAL(6,2) NOT NULL,
+        descricao TEXT NOT NULL,
+        FOREIGN KEY (id_pedido) REFERENCES pedido(id),
+        FOREIGN KEY (id_cardapio) REFERENCES cardapio(id)
+    );
+
+INSERT INTO motoboy VALUES
+(DEFAULT, "Marquinho Ruindade Pura", "Meiota"),
+(DEFAULT, "Clebinho Humildade Pura", "Ninja");
+
+INSERT INTO cardapio VALUES
+(DEFAULT,"Marmita de Picanha", 18.00, "Arroz, Feijao, Picanha, Fritas e Repolho"),
+(DEFAULT,"Marmita de Feijoada", 15.00, "Arroz, Feijao preto, Fritas, Farofa e Couve"),
+(DEFAULT,"Suco de maracujá", 5.00, "Delicioso suco de maracujá"),
+(DEFAULT,"Refrigerante 2 litros", 8.00, "Refrigerante dois litros");
+
+INSERT INTO cliente VALUES
+(DEFAULT, "Jair Messias", "1928259123", "Planalto, 22"),
+(DEFAULT, "Luis Inacio", "1912351251", "Triplex, 13"),
+(DEFAULT, "Jacinto Dores", NULL, NULL),
+(DEFAULT, "Aquino Dente", NULL, NULL);
+
+
+INSERT INTO pedido VALUES
+(DEFAULT, 1, 1, "9:00", NULL,CURDATE(),"Em preparo", "Sem picles", 1),
+(DEFAULT, 3, NULL, "10:00","11:00",CURDATE(),"Pronto", NULL, 0),
+(DEFAULT, 4, NULL, "11:00", NULL,CURDATE(),"Em preparo", "Sem picles", 0),
+(DEFAULT, 2, 1, "12:00","12:15",CURDATE(),"Pronto", NULL, 1);
+
+INSERT INTO itens VALUES
+(1, 1, 10, "aaaa"),
+(2, 2, 10, "aaaa"),
+(2, 3, 10, "aaaa"),
+(3, 3, 10, "aaaa"),
+(4, 4, 10, "aaaa");
