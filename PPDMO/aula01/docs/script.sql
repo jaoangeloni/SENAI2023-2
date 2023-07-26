b96e8e5c0a4913ee4120d194923b7fa3ca63de37
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-07-26 09:03:55 -0300

    PROJ

--- PPDMO/aula01/docs/script.sql
+++ PPDMO/aula01/docs/script.sql
@@ -32,6 +32,7 @@
         obs VARCHAR(255),
         FOREIGN KEY(id_cliente) REFERENCES cliente(id),
         FOREIGN KEY(id_motoboy) REFERENCES motoboy(id),
+        total DECIMAL(6,2) NULL,
         entrega BOOLEAN
     );
 
@@ -39,7 +40,7 @@
     (
         id_pedido INT NOT NULL,
         id_cardapio INT NOT NULL,
-        total DECIMAL(6,2) NOT NULL,
+        preco DECIMAL(6,2) NOT NULL,
         descricao TEXT NOT NULL,
         FOREIGN KEY (id_pedido) REFERENCES pedido(id),
         FOREIGN KEY (id_cardapio) REFERENCES cardapio(id)
@@ -50,10 +51,12 @@ INSERT INTO motoboy VALUES
 (DEFAULT, "Clebinho Humildade Pura", "Ninja");
 
 INSERT INTO cardapio VALUES
-(DEFAULT,"Marmita de Picanha", 18.00, "Arroz, Feijao, Picanha, Fritas e Repolho"),
-(DEFAULT,"Marmita de Feijoada", 15.00, "Arroz, Feijao preto, Fritas, Farofa e Couve"),
+(DEFAULT,"Picanha ao alho", 18.00, "Arroz, Feijao, Picanha, Fritas e Repolho"),
+(DEFAULT,"Feijoada", 15.00, "Arroz, Feijao preto, Fritas, Farofa e Couve"),
+(DEFAULT,"Frango á Milanesa", 15.00, "Arroz, Frango Empanado, Fritas  e Couve"),
 (DEFAULT,"Suco de maracujá", 5.00, "Delicioso suco de maracujá"),
-(DEFAULT,"Refrigerante 2 litros", 8.00, "Refrigerante dois litros");
+(DEFAULT,"Suco de uva", 5.00, "Delicioso suco de uva"),
+(DEFAULT,"Suco de limão", 5.00, "Deliciosa limonada");
 
 INSERT INTO cliente VALUES
 (DEFAULT, "Jair Messias", "1928259123", "Planalto, 22"),
@@ -63,14 +66,14 @@ INSERT INTO cliente VALUES
 
 
 INSERT INTO pedido VALUES
-(DEFAULT, 1, 1, "9:00", NULL,CURDATE(),"Em preparo", "Sem picles", 1),
-(DEFAULT, 3, NULL, "10:00","11:00",CURDATE(),"Pronto", NULL, 0),
-(DEFAULT, 4, NULL, "11:00", NULL,CURDATE(),"Em preparo", "Sem picles", 0),
-(DEFAULT, 2, 1, "12:00","12:15",CURDATE(),"Pronto", NULL, 1);
+(DEFAULT, 1, 1, "9:00", NULL,CURDATE(),"Em preparo", "Sem picles", "", 1),
+(DEFAULT, 3, NULL, "10:00","11:00",CURDATE(),"Pronto", NULL, "", 0),
+(DEFAULT, 4, NULL, "11:00", NULL,CURDATE(),"Em preparo", "Sem picles", "", 0),
+(DEFAULT, 2, 1, "12:00","12:15",CURDATE(),"Pronto", NULL, "", 1);
 
 INSERT INTO itens VALUES
-(1, 1, 10, "aaaa"),
-(2, 2, 10, "aaaa"),
-(2, 3, 10, "aaaa"),
-(3, 3, 10, "aaaa"),
-(4, 4, 10, "aaaa");
+(1, 1, 10, ""),
+(2, 2, 10, ""),
+(2, 3, 10, ""),
+(3, 3, 10, ""),
+(4, 4, 10, "");
